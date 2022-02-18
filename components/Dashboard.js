c37bb504592cef8990383ebffd6e6f4113ce8743
Stephanie <stephaniiehallberg@gmail.com>
2022-02-18 22:52:08 +0100

    template, sidebar complete

---
+++ components/Dashboard.js
@@ -0,0 +1,13 @@
+import Sidebar from './Sidebar'
+import Body from './Body'
+import Right from './Right'
+
+export default function Dashboard() {
+  return (
+    <main>
+      <Sidebar />
+      <Body />
+      <Right />
+    </main>
+  )
+}
