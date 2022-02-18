03b1f66ce6825fdaf5f95614cce419280d1970d4
Stephanie <stephaniiehallberg@gmail.com>
2022-02-18 22:25:31 +0100

    commit setup

---
+++ validate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npx concurrently \
+  --kill-others-on-fail \
+  --prefix "[{name}]" \
+  --names "prettier,lint,typecheck" \
+  --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white" \
+    "npm run prettier --loglevel silent" \
+    "npm run lint" \
