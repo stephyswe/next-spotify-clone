8e39bf6acb2df26da89a014ee3aeceddb10fc57b
Stephanie <stephaniiehallberg@gmail.com>
2022-02-19 10:26:19 +0100

    find spotify songs in body component

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
 import Search from './Search'
 
-export default function Body() {
+export default function Body({ spotifyApi }) {
+  const { data: session } = useSession()
+  const accessToken = session.accessToken
   const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const [newReleases, setNewReleases] = useState([])
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  // Searching...
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accessToken) return
+
+    let cancel = false
+
+    spotifyApi.searchTracks(search).then((res) => {
+      setSearchResults(res)
+    })
+  }, [search, accessToken])
+
+  console.log(searchResults)
+
   return (
     <section className="ml-24 flex-grow space-y-8 bg-black py-4 md:mr-2.5 md:max-w-6xl">
       <Search search={search} setSearch={setSearch} />
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,12 +1,17 @@
+import SpotifyWebApi from 'spotify-web-api-node'
 import Sidebar from './Sidebar'
 import Body from './Body'
 import Right from './Right'
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+})
 
 export default function Dashboard() {
   return (
     <main className="flex min-h-screen min-w-max bg-black">
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} />
       <Right />
     </main>
   )
